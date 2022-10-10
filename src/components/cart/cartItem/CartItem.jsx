43167fe9b685c1d7998eaf82481c5666bfbc26ea
Ksenia <kkandaurova@gmail.com>
2022-10-10 16:43:35 +0300

    Change homepage

--- src/components/cart/cartItem/CartItem.jsx
+++ src/components/cart/cartItem/CartItem.jsx
@@ -1,16 +1,16 @@
 import style from "./cartItem.module.css"
 
 const CartItem = (props) => {
-    return(
+    return (
         <div className={style['cart-item']}>
-        <img className={style['cart-img']} src={props.img} alt="" />
-        <h3 className={style['cart-title']}>
-            {props.title}
-            <br />
-            <span className={style['cart-price']}>{props.price} руб.</span>
-        </h3>
-        <button className={style['close-btn']}>X</button>
-    </div>
+            <img className={style['cart-img']} src={props.img} alt="" />
+            <h3 className={style['cart-title']}>
+                {props.title}
+                <br />
+                <span className={style['cart-price']}>{props.price} руб.</span>
+            </h3>
+            <button className={style['close-btn']}>X</button>
+        </div>
     )
 }
 
